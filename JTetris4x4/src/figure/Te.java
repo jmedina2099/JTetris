1342a9b06f4b7fd0eebde786a355c11875491b4f
jmedina <jmedina@debian>
2021-06-10 14:38:12 -0500

    The T

---
+++ JTetris4x4/src/figure/Te.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package figure;
+
+import java.awt.Point;
+
+/**
+ * @author jmedina
+ *
+ */
+public class Te extends Figure {
+
+	public Te() {
+		System.out.println( "Te" );
+		init();
+	}
+
+	private void init() {
+		int x = 3;
+		int y = 0;
+		
+		Point[] puntos = new Point[4];
+		puntos[0] = new Point(x,y);
+		puntos[1] = new Point(x+1,y);
+		puntos[2] = new Point(x+1,y-1);
+		puntos[3] = new Point(x+2,y);
+
+		for( Point p : puntos ) {
+			figura.add(p);
+		}
+	}
+
+}
